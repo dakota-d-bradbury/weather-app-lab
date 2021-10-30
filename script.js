f1c24ead3cb7d31f4fdf40de5a10b09f5c75ddf2
Dakota GA <dakotwageneralassembly@Dakotas-MacBook-Pro-2.local>
2021-10-30 00:35:12 -0500

    WIP: Testing AJAX connection

--- script.js
+++ script.js
@@ -1,47 +1,7 @@
 // Constants - Data that does not change
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+const API_KEY = 'd665df7391c8bb868fde13c4733f7668';
 
-// Variables - Data that changes
 
-// Cached Element References
-const $title = $('#title');
-const $year = $('#year');
-const $rated = $('#rated');
-const $form = $('form');
-const $input = $('input[type="text"]');
-
-// Event Listeners
-$form.on('submit', handleSubmit);
-// Functions
-
-function handleSubmit(evt) {
-    evt.preventDefault(); // this turns off the default page refresh behavior in the browser
-    // grab the movie title we're submitting in the form
-    const movieTitle = $input.val();
-    // use that movie to request a specific movie
-    console.log(movieTitle)
-
-    // Using jQuery's $.ajax() method
-    // We use it to request information from a resource using AJAX
-    // This method, when called, returns a JavaScript promise Object
-    
-    
-    // the .then() method is how we assign instructions
-    // to the promise object so we can tell it what to do
-    // when the promise is fultilled
-    $.ajax('https://www.omdbapi.com/?apikey=5ceecba4&t=' + movieTitle)
-    .then(function(data) {
-        // success callback function
-        console.log('promise fulfilled');
-        console.log(data);
-        
-        // add data as text content to our DOM elements
-        $title.text(data.Title);
-        $year.text(data.Year);
-        $rated.text(data.Rated);
-    
-    }, function(error) {
-        // failure callback function
-        console.log('promise failed');
-        console.log(error);
-    });
-}
+    const weatherLocation = 'Dallas'//$input.val();
+  const promise =  $.ajax(`${BASE_URL}weather?q=`+weatherLocation+`&appid=${API_KEY}`)
